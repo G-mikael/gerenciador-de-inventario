948b1b5fb7b614312b7f39a806e46b0fd9ec1ff1
Fidel Luis Como <99187459+FidelComo@users.noreply.github.com>
2022-10-10 08:57:49 -0300

    Update gerenciador.py

--- gerenciador/gerenciador.py
+++ gerenciador/gerenciador.py
@@ -26,6 +26,14 @@ def adicionar_item(item, valor, inv):
     :return: Dicionário com o item a mais
     :rtype: dict
     '''
+    inv = inv.copy()
+    
+    if item not in inv:
+        inv[item]=valor
+        return inv
+    else:
+        raise Exception('O item ja está no inventario')
+    
     
 def remover_item(chave, inv):
     '''A função recebe um dicionário e uma chave e remove um ítem do dicionário a
@@ -39,15 +47,29 @@ def remover_item(chave, inv):
     :rtype: dict
     '''
 
-def busca_item(chave):
+def busca_item(chave, inv):
     '''
     A função recebe uma chave busca o item referente aquela chave e retorna
     o item e chave dado
     
     :param chave: chave do item que será buscado
     :chave type: str
+    :param inv : dicionário no qual será buscado o item
+    :inv type: dict
     :return: Dicionário com a chave e o value do item buscado
     :rtype: dict
 
     '''
+    if chave in inv:
+        return {chave:inv[chave]}
+    else:
+        raise Exception('O item não está no inventario')
     
+dic = {'a':1, 'b':2, 'c':3}
+print(dic)
+#print(adicionar_item('a', 4, dic))
+dic = adicionar_item('d', 4, dic)
+print(dic)
+print(busca_item('a', dic))
+print(busca_item('d', dic))
+
