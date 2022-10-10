b4728f9e8b00a31b55fbb45110fd9973b05ad189
Gerardo Mikael do Carmo Pereira <B46994@fgv.edu.br>
2022-10-10 08:32:45 -0300

    criar gerenciador

---
+++ gerenciador/gerenciador.py
@@ -0,0 +1,53 @@
+import numpy
+import pandas
+
+def listar_inventario(inv):
+    '''A função recebe como parâmetro um dicionário e 
+    retorna um dataframe com todos os ítens e seus valores
+
+
+    :param inv : dicionário com itens e valores
+    :inv type: dict
+    :return: Dataframe
+    :rtype: Pandas.Dataframe
+    '''
+    
+def adicionar_item(item, valor, inv):
+    '''A função recebe como parâmetro um dicionário e 
+    retorna um dataframe com todos os ítens e seus valores
+
+
+    :param item: Nome do item a ser adicionado como 
+    :inv type: str
+    :param item: Valor do item a ser adicionado como value
+    :inv type: str
+    :param inv : dicionário ao qual será adicionado o item
+    :inv type: dict
+    :return: Dicionário com o item a mais
+    :rtype: dict
+    '''
+    
+def remover_item(chave, inv):
+    '''A função recebe um dicionário e uma chave e remove um ítem do dicionário a
+    partir da chave dada
+    
+    :param chave: chave do item queserá removido 
+    :chave type: str
+    :param inv : dicionário ao qual será removido o item
+    :inv type: dict
+    :return: Dicionário com o item a mais
+    :rtype: dict
+    '''
+
+def busca_item(chave):
+    '''
+    A função recebe uma chave busca o item referente aquela chave e retorna
+    o item e chave dado
+    
+    :param chave: chave do item que será buscado
+    :chave type: str
+    :return: Dicionário com a chave e o value do item buscado
+    :rtype: dict
+
+    '''
+    
